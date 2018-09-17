dbb077f125314e5c1c1d32072d8901c6398b52da
Tayyab <tayyab.bashir26@gmail.com>
2018-09-17 17:17:29 +1000

    Uptil pg 20

--- src/com/HeadFirstJava/Main.java
+++ src/com/HeadFirstJava/Main.java
@@ -2,6 +2,28 @@ public class Main {
     public static void main(String[] args){
 		
 		System.out.println("HeadFirstJava First Chapter");
-
-    }
+	
+		double y = 26.5;
+		int x = (int) y;
+		System.out.println(y);
+		
+		
+		 x = 3;
+		while (x > 0) {
+			if (x == 1) {
+				System.out.print("d");
+				x = x - 1;
+			}
+			if (x == 2) {
+				System.out.print("b c");
+				}
+			if (x > 2) {
+				System.out.print("a");
+				
+			}
+			x = x - 1;
+			System.out.print("-");
+		}
+		
+	}
 }
