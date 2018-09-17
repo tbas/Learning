bf6c491a0c1cc0bfad36726c850b1586445f48ed
Tayyab <tayyab.bashir26@gmail.com>
2018-09-17 13:01:24 +1000

    removed the package

--- src/com/HeadFirstJava/Main.java
+++ src/com/HeadFirstJava/Main.java
@@ -1,7 +1,7 @@
-package com.HeadFirstJava;
-
 public class Main {
-    public static void main(String[] argss){
+    public static void main(String[] args){
+		
+		System.out.println("HeadFirstJava First Chapter");
 
     }
 }
