1ceb17211d8b531a79f1df6208fefa777208d462
Tayyab <tayyab.bashir26@gmail.com>
2018-09-18 16:10:23 +1000

    added package names

--- src/com/Chap1/MyFirstApp.java
+++ src/com/Chap1/MyFirstApp.java
@@ -1,5 +1,33 @@
-public class MyFirstApp{
-		public static void main (String[] args){
-		System.out.println("I rule");
+package com.Chap1;
+
+public class MyFirstApp {
+    public static void main(String [] args){
+		
+		System.out.println("HeadFirstJava First Chapter s");
+	
+		float y = 26.5f;
+		int x = (int) y;
+		System.out.println(x);
+		
+		x = 3;
+		if (x > 2) {
+			System.out.print("a");
+		
+		}
+		while (x > 0) {
+					
+			x = x - 1;
+			System.out.print("-");
+			
+			if (x == 1) {
+				System.out.print("d");
+				x = x - 1;
+			}
+			if (x == 2) {
+				System.out.print("b c");
+				}
+
+		}
+		
 	}
 }
--- src/com/HeadFirstJava/Main.java
+++ src/com/HeadFirstJava/Main.java
@@ -1,29 +1,8 @@
+package com.HeadFirstJava;
+
 public class Main {
     public static void main(String[] args){
 		
-		System.out.println("HeadFirstJava First Chapter");
-	
-		double y = 26.5;
-		int x = (int) y;
-		System.out.println(y);
-		
-		
-		 x = 3;
-		while (x > 0) {
-			if (x == 1) {
-				System.out.print("d");
-				x = x - 1;
-			}
-			if (x == 2) {
-				System.out.print("b c");
-				}
-			if (x > 2) {
-				System.out.print("a");
-				
-			}
-			x = x - 1;
-			System.out.print("-");
-		}
-		
+		System.out.println("Main for HeadFirstJava");
 	}
 }
